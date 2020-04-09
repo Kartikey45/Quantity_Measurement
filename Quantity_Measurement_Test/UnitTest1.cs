3e4f6f40182cfdb830b4edf5184b7265dacc0052
Kartikey45 <singhkartikey45@gmail.com>
2020-04-09 14:45:12 +0530

    ADDED : TC-1.3 Reference check

--- Quantity_Measurement_Test/UnitTest1.cs
+++ Quantity_Measurement_Test/UnitTest1.cs
@@ -6,7 +6,7 @@ namespace Quantity_measurement_problem_Test
     public class Tests
     {
         [Test]
-        public void Test1()
+        public void Test1()  //TC 1.1
         {
             FeetComparision compare = new FeetComparision();
             int result = compare.feetcompare("0");
@@ -14,7 +14,7 @@ namespace Quantity_measurement_problem_Test
         }
 
         [Test]
-        public void Test2()
+        public void Test2()  //TC 1.2
         {
             FeetComparision compare = new FeetComparision();
             try
@@ -25,6 +25,18 @@ namespace Quantity_measurement_problem_Test
             {
                 Assert.AreEqual(CustomException.ExceptionType.INPUT_NULL, e.type);
             }
+        }
+
+        [Test]
+        public void Test3() //TC 1.3
+        {
+            FeetComparision compare = new FeetComparision();
+            compare.feetcompare("0");
+            FeetComparision compar2 = new FeetComparision();
+            compar2.feetcompare("0");
+            compare = compar2;
+            bool areEqual = ReferenceEquals(compare, compar2);
+            Assert.IsTrue(areEqual);
         }
     }
 }
